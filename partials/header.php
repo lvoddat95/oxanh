c414844b545015e861dcfc2a83435a4029216223
Dat Lv <lvoddat95@yahoo.com>
2022-12-22 09:22:57 +0700

    update fix

--- partials/header.php
+++ partials/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#115197" />
     <title>Nền tảng tương trợ bệnh hiểm nghèo | Công ty Cổ phần Công nghệ Internet Ô Xanh </title>
     <link rel="icon" type="image/x-icon" href="<?php echo SOURCE; ?>assets/img/favicon.ico">
     <!-- Load Font -->
